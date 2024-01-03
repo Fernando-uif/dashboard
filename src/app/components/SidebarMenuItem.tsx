39a46d50749a5251c747f0a0c7e373071d79d4ef
Fernando Sosa Torres <fsosa@televisa.news>
2024-01-02 21:00:32 -0600

    add pages and components

---
+++ src/app/components/SidebarMenuItem.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+interface Props {
+  path: string;
+  icon: JSX.Element;
+  title: string;
+  subTitle: string;
+}
+
+//  bg-blue-800
+const SidebarMenuItem = ({ icon, path, subTitle, title }: Props) => {
+  const pathName = usePathname();
+  console.log(pathName,'pathname');
+  console.log(path,'path mandado');
+  return (
+    <Link
+      href={path}
+      className={`w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 hover:bg-white/5 transition ease-linear duration-150 ${
+        path === pathName ? "bg-blue-800" : ""
+      }`}
+    >
+      <div>{icon}</div>
+      <div className="flex flex-col">
+        <span className="text-lg font-bold leading-5 text-white">{title}</span>
+        <span className="text-sm text-white/50 hidden md:block">
+          {subTitle}
+        </span>
+      </div>
+    </Link>
+  );
+};
+
+export default SidebarMenuItem;
